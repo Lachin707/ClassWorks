f49f03da032726a4bca664ce8b43781462462112
Lachin707 <tu7da4k8x@code.edu.az>
2023-10-23 13:48:56 +0400

    update

---
+++ task 21/main.js
@@ -0,0 +1,18 @@
+let array = ["banana", "apple", "cherry"];
+
+let bd = document.querySelector("body");
+
+let btn = document.querySelector("button");
+let dv = document.createElement("div");
+let hd = document.createElement("h3");
+dv.style.width = "100px";
+dv.style.height = "100px";
+dv.style.background = "red";
+document.body.appendChild(dv);
+
+btn.addEventListener("click", (event) => {
+	//bd.innerHTML += `<div>hello</div>`;
+	
+	//let dv = document.querySelector("div");
+
+});
