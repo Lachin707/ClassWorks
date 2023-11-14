55515d96e99451ff9d898dd698ae89ccb33c0106
Lachin707 <tu7da4k8x@code.edu.az>
2023-11-14 12:05:21 +0400

    delete folders

--- 28. React 1 task (components, fragments, vite, vercel)/vite-project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <>
-        <h1>this is Footer</h1>
-        </>
-    )
-}
-
-export default Footer;
--- task 31/vite-project/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <>
-        <h1>this is Header</h1>
-        </>
-    )
-}
-
-export default Header;
