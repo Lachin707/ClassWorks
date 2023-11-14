d967c3ffb8c27ed5004b97d4ad7488b95182092e
Lachin707 <tu7da4k8x@code.edu.az>
2023-11-14 11:34:42 +0400

    task 31 upload

---
+++ task 31/vite-project/src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button"
+
+function AboutUs() {
+    return (
+        <>
+        <h1>this is About us page</h1>
+        <Button></Button>
+        </>
+    )
+}
+
+export default AboutUs;
---
+++ task 31/vite-project/src/components/students.js
@@ -0,0 +1,18 @@
+export let array = [
+    {
+        id: 0,
+        name: "Lachin"
+    },
+    {
+        id: 1,
+        name: "Lachin"
+    },
+    {
+        id: 2,
+        name: "Lachin"
+    },
+    {
+        id: 3,
+        name: "Lachin"
+    }
+]
