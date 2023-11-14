ea8a0c94ae6a62396dda0a85685b97eb9bb56b53
Lachin707 <tu7da4k8x@code.edu.az>
2023-11-14 11:54:06 +0400

    test task 28

---
+++ 28. React 1 task (components, fragments, vite, vercel)/vite-project/src/components/Slider.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function Slider() {
+    return (
+        <>
+        <h1>this is Slider</h1>
+        <Button></Button>
+        <Button></Button>
+        </>
+    )
+}
+
+export default Slider;
